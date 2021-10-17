e1ba8ff0a75d349323072d8674e2c595e8bc75bf
Alexey Dunin <valentez.alkalinella@gmail.com>
2021-10-17 18:26:07 +0300

    First exec complete

---
+++ hw11.py
@@ -0,0 +1,6 @@
+a = "wolf"
+b = "walkers"
+print(f'Lets talk about {a}{b} ')
+r = int(input("Yours rating is(only numbers)? "))
+o = input("Yours opinion is? ")
+print(f"Fairy tail about {a}s for you is {r}/10 amd {o}")
